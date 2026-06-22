5f2b5dbd3d039432940de5a48a17006db2805957
SVFuture <dosadcuk642@gmail.com>
2026-06-22 13:29:20 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,55 +1,94 @@
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
-class Point {
-public:
-    double x;
-    double y;
-    double z;
+class Fraction {
+private:
+    int numerator;
+    int denominator;
 
-    Point() {
-        x = 0.0;
-        y = 0.0;
-        z = 0.0;
+public:
+    Fraction() {
+        numerator = 0;
+        denominator = 1;
     }
 
     void input() {
-        cout << "Write x, y, z (with space): ";
-        cin >> x >> y >> z;
+        cout << "Enter numerator: ";
+        cin >> numerator;
+        cout << "Enter denominator: ";
+        cin >> denominator;
+        
+        while (denominator == 0) {
+            cout << "Error! Denominator cannot be 0. Enter again: ";
+            cin >> denominator;
+        }
     }
 
     void print() {
-        cout << "Point: (" << x << ", " << y << ", " << z << ")\n";
+        cout << numerator << "/" << denominator << endl;
     }
 
-    void saveToFile(const char* filename) {
-        ofstream out(filename);
-        out << x << " " << y << " " << z;
-        out.close();
+    Fraction add(Fraction other) {
+        Fraction result;
+        result.numerator = (this->numerator * other.denominator) + (other.numerator * this->denominator);
+        result.denominator = this->denominator * other.denominator;
+        return result;
     }
 
-    void loadFromFile(const char* filename) {
-        ifstream in(filename);
-        in >> x >> y >> z;
-        in.close();
+    Fraction subtract(Fraction other) {
+        Fraction result;
+        result.numerator = (this->numerator * other.denominator) - (other.numerator * this->denominator);
+        result.denominator = this->denominator * other.denominator;
+        return result;
+    }
+
+    Fraction multiply(Fraction other) {
+        Fraction result;
+        result.numerator = this->numerator * other.numerator;
+        result.denominator = this->denominator * other.denominator;
+        return result;
+    }
+
+    Fraction divide(Fraction other) {
+        Fraction result;
+        if (other.numerator == 0) {
+            cout << "Error! Division by zero." << endl;
+            return result;
+        }
+        result.numerator = this->numerator * other.denominator;
+        result.denominator = this->denominator * other.numerator;
+        return result;
     }
 };
 
 int main() {
-    Point p; 
+    Fraction fraction1;
+    Fraction fraction2;
 
-    p.input();
-    p.print();
+    cout << "Entering the first fraction" << endl;
+    fraction1.input();
 
-    p.saveToFile("point.txt");
+    cout << "\n Entering the second fraction" << endl;
+    fraction2.input();
 
-    cout << "Changed coordinate x: " << p.x << "\n";
+    cout << "\nFractions entered:" << endl;
+    cout << "Fraction 1: "; fraction1.print();
+    cout << "Fraction 2: "; fraction2.print();
+    
+    cout << "\n Results" << endl;
 
-    p.loadFromFile("point.txt");
-    cout << "Data loaded from file: ";
-    p.print();
+    Fraction sum = fraction1.add(fraction2);
+    cout << "Sum: "; sum.print();
+
+    Fraction diff = fraction1.subtract(fraction2);
+    cout << "Difference: "; diff.print();
+
+    Fraction prod = fraction1.multiply(fraction2);
+    cout << "Product: "; prod.print();
+
+    Fraction quot = fraction1.divide(fraction2);
+    cout << "Quotient: "; quot.print();
 
     return 0;
 }
